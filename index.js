9c7ea99812fc92e3de92feb6602b37a953f48a53
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2016-04-26 14:02:05 +0200

    library working

---
+++ index.js
@@ -0,0 +1,24 @@
+
+
+module.exports = function(sails) {
+    
+    var loader = require('sails-util-mvcsloader')(sails);
+    
+    loader.configure({
+        policies: __dirname + '/api/policies',// Path to the policies to load
+        config: __dirname + '/config' // Path to the config to load
+    });
+    
+    var search = require('./lib/search.js');
+    var exec = require('./lib/exec.js');
+    
+    return {
+        search: search,
+        exec: exec,
+        initialize: function(next){
+            loader.injectControllers( __dirname + '/api/controllers', function(err){
+                next(err);
+            });
+        }
+    };
+};
